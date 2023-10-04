1dbb5f2cc41c0da34aa1bd41e3f81d7883f31bb4
guzmanbr <guzmanghu@gmail.com>
2023-10-04 15:51:03 +0000

    clase

---
+++ Tema2/Practicas/Practica2/HolaMundoIdioma.php
@@ -0,0 +1,12 @@
+<?php
+    $es = "Buenos días";
+    $por = "Bom dia";
+    $ita = "Buongiorno";
+    $gri = "Καλημέρα";
+    $en = "Good Morning";
+
+    $elegido = $_GET["idioma"];
+
+    echo $$elegido;
+    
+?>
---
+++ Tema2/Practicas/Practica2/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Idiomas</title>
+
+        <style>
+            body {
+                text-align: center;
+            }
+
+            img {
+                height: 200px;
+                width: 350px;
+                margin: 10px;
+                border: 1px solid black;
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <a href="HolaMundoIdioma.php?idioma=es"><img src="imagenes/España.jpg" alt=""><br>Español</a>
+        <br>
+        <a href="HolaMundoIdioma.php?idioma=por"><img src="imagenes/Portugal.jpg" alt=""><br>Portugués</a>
+        <br>
+        <a href="HolaMundoIdioma.php?idioma=ita"><img src="imagenes/Italia.jpg" alt=""><br>Italiano</a>
+        <br>
+        <a href="HolaMundoIdioma.php?idioma=gri"><img src="imagenes/Grecia.jpg" alt=""><br>Griego</a>
+        <br>
+        <a href="HolaMundoIdioma.php?idioma=en"><img src="imagenes/Gran Bretaña.jpg" alt=""><br>Inglés</a>
+    </body>
+</html>
