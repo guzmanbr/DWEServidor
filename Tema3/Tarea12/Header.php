8281c8d3e53f247a17e940c37e3553dff90f4c5f
guzmanbr <guzmanghu@gmail.com>
2023-11-22 17:45:48 +0000

    clase

--- Tema3/Tarea12/Header.php
+++ Tema3/Tarea12/Header.php
@@ -4,42 +4,44 @@ class HeaderClase extends FPDF {
     //creo la funcion header
     function Header(){
         
-        $this->Image('./imagen.jpg',10,10,35,35);//nombre, posicion, ancho alto
+        $this->Image('./logo.jpg',6,11,38,38);//nombre, posicion, ancho alto
         //Nombre
-        $this->SetFont('Courier','B',25);//familia,estilo,tamaño
-        //$this->SetTextColor(100,80,200);//color
-        $this->setY(9);
+        $this->SetFont('Courier','',25);//familia,estilo,tamaño
+        $this->setY(10);
         $this->SetX(50);//centro
-        $this->Write(10,"FootShop");
+        $this->Write(10,"SneakerShop");
         $this->Ln();//ANADE UN SALTO DE LINEA
         //datos
+        $this->setY(22);
         $this->SetX(50);//centro
-        $this->SetFont('Courier','B',10);//familia,estilo,tamaño
-        $this->SetTextColor(118,28,28);//color
-        $this->Write(5,"Guzman Benito Rodriguez");
+        $this->SetFont('Courier','',12);//familia,estilo,tamaño
+        $this->Write(5,utf8_decode("Guzmán Benito Rodríguez"));
         $this->Ln();
         $this->SetX(50);//centro
         $this->Write(5,"DIF/NIF: 77777777K");
         $this->Ln();
-        $this->SetX(50);//centro
+        $this->SetX(50);
         $this->Write(5,"Calle Paraiso n7");
         $this->Ln();
         $this->SetX(50);//centro
         $this->Write(5,"CP 14700");
         $this->Ln();
         $this->SetX(50);//centro
-        $this->Write(5,"Cordoba Espana");
+        $this->Write(5,utf8_decode("Cordoba España"));
+        $this->SetLineWidth(0.5);
+        $this->Line( 118, 9, 118, 52);
     }
 
 
 
     function Footer(){
         //fuente
-        $this->SetFont('Courier','B',20);//familia,estilo,tamaño
-        $this->SetTextColor(100,80,200);//color
+        $this->SetFont('Courier','',20);//familia,estilo,tamaño
         $this->SetY(-20);//para ponerlo abajo
-        $this->SetX(-20);//para ponerlo a la izquierda
+        $this->SetX(80);//para ponerlo a la izquierda
         //añado texto
+        $this->Write(5,"SneakerShop");
+        $this->SetX(-20);//para ponerlo abajo
         $this->Write(5,$this->PageNo());
 
     }
--- Tema3/Tarea12/crearPDF.php
+++ Tema3/Tarea12/crearPDF.php
@@ -3,66 +3,128 @@
 require ('../../fpdf186/fpdf.php');
 require ('./Header.php');
 
-//creo nuevo objeto de FPDF
-
-$pdf = new HeaderClase();
-
-$pdf->AddPage();
-//Se genera header automaticamente
-
-$pdf->SetFont('Courier','B',10);//familia,estilo,tamaño
-$pdf->SetY(12);
-$pdf->SetX(130);
-$pdf->Cell(70,5,"Cliente",1,2,'C',false);
-$pdf->Cell(70,5,"Nombre:",0,2,'L',false);
-$pdf->Cell(70,5,"CIF/NIF:",0,2,'L',false);
-$pdf->Cell(70,5,"Calle:",0,2,'L',false);
-$pdf->Cell(70,5,"Codigo:",0,2,'L',false);
-$pdf->Cell(70,5,"Ciudad:",0,2,'L',false);
-
-
-
-//Añado otra pagina
-$pdf->AddPage();
-//añadir imagen
-$pdf->Image('./imagen.jpg',50,20,120,90);//nombre, posicion, ancho alto
-
-
-
-
-$pdf->Output();//Guardo
-
-
-
 
 $clientes = array(
-    array('pc1','Lenovo','1TB','4Gb RAM'),
-    array('pc2','Asus','2TB','8Gb RAM'),
-    array('pc3','Mac','2TB','4Gb RAM'),
-    array('pc4','Hp','1TB','8Gb RAM'),
+    array('Manuel Fernandez','88888888K','c/Mala n1','18190 Armilla', 'Granada'),
+    array('Raul Martinez','88888888K','c/Salamanca n1','19025 Salamanca', 'Salamanca'),
+    array('Alejandro Balde','88888888K','c/Valladolid n1','18190 Valladolid', 'Valladolid'),
+    array('Jose Ramos','88888888K','c/Soria n1','20190 Soria', 'Soria'),
+);
+
+$productos = array(
+    array('Nike Airforce 1',2,120,21),
+    array('Nike Air Max 95',1,150,12),
+    array('Adidas Forum',1,99,15),
+    array('Reebok Classic',1,80,21),
+    array('Adidas Samba',1,65,21),
 );
 
 
+//creo nuevo objeto de FPDF
+$pdf = new HeaderClase();
 
-function crearTabla($array,$pdf){
+//añado pagina
+$pdf->AddPage();
+
+//Creo la tabla del cliente
+crearCliente($clientes[1],$pdf);
+
+//Creo  la tabla de la factura
+crearTabla($productos,$pdf);
+
+//Guardo
+$pdf->Output();
 
 
-    $pdf->SetFont('Courier','B',10);//familia,estilo,tamaño
+function crearCliente($cliente,$pdf){
+
+    //fuente y posicion
+    $pdf->SetFont('Courier','',11);//familia,estilo,tamaño
     $pdf->SetY(12);
     $pdf->SetX(130);
-    $pdf->Cell(70,5,"Cliente",1,2,'C',false);
-    $pdf->Ln();//salto
-    $pdf->SetY(12);
-    $pdf->SetX(130);
+    //Creo las celdas predeterminadas en su posicion
+    $pdf->Cell(70,6,"Cliente",1,2,'C',false);
+    $pdf->Cell(70,6,"Nombre:",0,2,'L',false);
+    $pdf->Cell(70,6,"CIF/NIF:",0,2,'L',false);
+    $pdf->Cell(70,6,"Calle:",0,2,'L',false);
+    $pdf->Cell(70,6,"Codigo:",0,2,'L',false);
+    $pdf->Cell(70,6,"Ciudad:",0,2,'L',false);
+    $pdf->SetY(18);
+    $pdf->SetX(150);
+
     //por cada fila
-    foreach ($array as $row) {
+    foreach ($cliente as $dato) {
         //en cada fila vamos rellenando las celdas
-        foreach ($row as $dato) {
-            $pdf->Cell(45,10,$dato,1,0,'C',false);
+        $pdf->Cell(70,6,$dato,0,2,'L',false);
+    }
+}
+
+
+
+function crearTabla($productos,$pdf){
+
+    //fuente y posicion
+    $pdf->SetFont('Courier','',11);//familia,estilo,tamaño
+    $pdf->SetY(90);
+    $pdf->SetX(10);
+    $pdf->SetFillColor(100,180,255);
+    //Creo las celdas predeterminadas en su posicion
+    $pdf->Cell(70,6,"Descripcion",1,0,'C',false);
+    $pdf->Cell(30,6,"Cantidad",1,0,'C',false);
+    $pdf->Cell(30,6,"Precio Ud",1,0,'C',false);
+    $pdf->Cell(30,6,"Importe",1,0,'C',false);
+    $pdf->Cell(30,6,"IVA",1,1,'C',false);
+
+    //inicializo los totales
+    $totalIva=0;
+    $totalCompra=0;
+
+    foreach ($productos as $producto) {
+
+        $conta = 0;
+        $cantidad=0;
+        $precioUD=0;
+        $totalArticulo=0;
+        $ivaArticulo=0;
+
+
+        foreach ($producto as $dato) {
+    
+            if ($conta==0) {
+                $pdf->Cell(70,6,$dato,1,0,'C',false);
+            }elseif ($conta==1) {
+                $pdf->Cell(30,6,$dato,1,0,'C',false);
+                $cantidad=$dato;//guardo la cantidad
+            }elseif ($conta==2) {
+                $pdf->Cell(30,6,($dato."$"),1,0,'C',false);
+                $precioUD=$dato;//guardo el precio de la unidad
+                $totalArticulo=$precioUD*$cantidad;//precio total de la suma de articulos
+                $pdf->Cell(30,6,($totalArticulo."$"),1,0,'C',false);
+            }elseif ($conta==3) {
+                $ivaArticulo=(($totalArticulo*$dato)/100);//calculo el iva de cada articulo
+                $pdf->Cell(30,6,($dato."%(".$ivaArticulo."$)"),1,1,'C',false);
+            }
+
+            //sumo los totales
+            $totalIva+=$ivaArticulo;
+            $totalCompra+=$totalArticulo;
+
+            $conta++;
         }
-        $pdf->Ln();
     }
 
+    //posicion y añado los precios totales
+    $pdf->Ln();
+    $pdf->SetY(210);
+    $pdf->SetX(40);
+    $pdf->Cell(70,6,"Total Impunible: ".$totalCompra."$",0,0,'C',false);
+    $pdf->Cell(70,6,"Total IVA: ".$totalIva."$",0,0,'C',false);
+    $pdf->Ln();
+    $pdf->SetY(220);
+    $pdf->SetX(75);
+    $pdf->Line( 20, 218, 180, 218);
+    $pdf->SetFont('Courier','B',13);//familia,estilo,tamaño
+    $pdf->Cell(70,6,"Total: ".$totalIva+$totalCompra."$",0,0,'C',false);
 
 }
 
